1f7b7b069c091f8215f988b28dd28f043d0fbe68
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 02:02:04 +0300

    Add bson attribute

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
+
+public class ProductImages
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ProductImagesId { get; set; }
+}

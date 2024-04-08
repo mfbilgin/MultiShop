9f8263409a8f56b537123a7c6f368716db454af7
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 02:13:46 +0300

    Revert "Add bson attribute"
    This reverts commit 1f7b7b069c091f8215f988b28dd28f043d0fbe68.

--- Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductDetail.cs
@@ -1,12 +1,7 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace MultiShop.Catalog.Entities;
+﻿namespace MultiShop.Catalog.Entities;
 
 public class ProductDetail
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
     public string ProductDetailId { get; set; }
     public string ProductDescription { get; set; }
     public string ProductInfo { get; set; }

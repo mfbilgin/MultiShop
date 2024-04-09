54fa47c2c0e04acd9460ddd4e93743264ada392d
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 19:02:40 +0300

    Create IProductImageService.cs

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImage/IProductImageService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.ProductImage;
+
+namespace MultiShop.Catalog.Services.ProductImage;
+
+public interface IProductImageService
+{
+    Task<List<ProductImageResultDto>> GetAllAsync();
+    Task<ProductImageGetByIdDto> GetByIdAsync(string productImageId);
+    Task CreateAsync(ProductImageCreateDto productImageCreateDto);
+    Task UpdateAsync(ProductImageUpdateDto productImageUpdateDto);
+    Task DeleteAsync(string productImageId);
+}

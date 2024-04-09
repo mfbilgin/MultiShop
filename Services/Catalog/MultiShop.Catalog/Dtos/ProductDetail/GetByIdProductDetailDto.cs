ca60ebaadf8d07c0da60a86bd28547ce9bae3f36
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 05:10:16 +0300

    Create GetByIdProductDetailDto.cs

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetail/GetByIdProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetail;
+
+public sealed class GetByIdProductDetailDto
+{
+    public string ProductDetailId { get; set; } = string.Empty;
+    public string ProductDescription { get; set; } = string.Empty;
+    public string ProductInfo { get; set; } = string.Empty;
+    public string ProductId { get; set; } = string.Empty;
+}

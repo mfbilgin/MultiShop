af5dc46177bd654600c0ca6978a4a8b19f0f471c
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-10 00:24:50 +0300

    Create DapperContext.cs

---
+++ Services/Discount/MultiShop.Discount/Context/DapperContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Discount.Entities;
+
+namespace MultiShop.Discount.Context;
+
+public class DapperContext(IConfiguration configuration) : DbContext
+{
+    private readonly string? _connectionString = configuration.GetConnectionString("DefaultConnection");
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=MFBILGIN;Initial Catalog=MultiShopDiscountDatabase;Integrated Security=true;TrustServerCertificate=True");
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+    public IDbConnection CreateConnection()=> new SqlConnection(_connectionString);
+}

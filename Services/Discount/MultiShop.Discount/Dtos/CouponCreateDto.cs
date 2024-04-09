6eacf1f31291270e5db6cc0964eba0448e7d80cb
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-10 00:36:26 +0300

    Create coupon crud dtos

---
+++ Services/Discount/MultiShop.Discount/Dtos/CouponCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Discount.Dtos;
+
+public sealed class CouponCreateDto
+{
+    public string CouponCode { get; set; } = string.Empty;
+    public int Rate { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime ValidDate { get; set; }
+}
---
+++ Services/Discount/MultiShop.Discount/Dtos/CouponResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Discount.Dtos;
+
+public sealed class CouponResultDto
+{
+    public int CouponId { get; set; }
+    public string CouponCode { get; set; } = string.Empty;
+    public int Rate { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime ValidDate { get; set; }
+}

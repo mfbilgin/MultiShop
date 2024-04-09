6652f3edb50a03cccaae1e3f4b653045a6902259
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 05:44:45 +0300

    Create ICategoryService.cs

---
+++ Services/Catalog/MultiShop.Catalog/Services/Category/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.Category;
+
+namespace MultiShop.Catalog.Services.Category;
+
+public interface ICategoryService
+{
+    Task<List<CategoryResultDto>> GetAllAsync();
+    Task<CategoryGetByIdDto> GetByIdAsync(string categoryId);
+    Task CreateAsync(CategoryCreateDto categoryCreateDto);
+    Task UpdateAsync(CategoryUpdateDto categoryUpdateDto);
+    Task DeleteAsync(string categoryId);
+}

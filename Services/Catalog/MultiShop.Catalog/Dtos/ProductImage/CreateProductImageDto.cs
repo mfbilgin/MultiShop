425b42bfc15e17a6632abc4c69af44fd3528ea8d
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 05:09:56 +0300

    Create CreateProductImageDto.cs

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductImage/CreateProductImageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductImage;
+
+public sealed class CreateProductImageDto
+{
+    public List<string> Images { get; set; } = [];
+
+    public string ProductId { get; set; } = string.Empty;
+}

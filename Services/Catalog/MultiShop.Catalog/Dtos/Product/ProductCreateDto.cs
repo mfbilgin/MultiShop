be3d8f550b19b590caeaacbdb620b931f3722b60
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 04:43:14 +0300

    Create ProductCreateDto.cs

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/Product/ProductCreateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MultiShop.Catalog.Dtos.Product;
+
+public class ProductCreateDto
+{
+    public string ProductName { get; set; }
+
+    public decimal ProductPrice { get; set; }
+    
+    public int ProductStock { get; set; }
+    
+    public string ProductImageUrl { get; set; }
+    
+    public string ProductDescription { get; set; }
+
+    public string CategoryId { get; set; }
+}

80c125e09d45741f2849e8694c8eb9ec549870a0
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 19:02:53 +0300

    Create IProductService.cs

---
+++ Services/Catalog/MultiShop.Catalog/Services/Product/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using MultiShop.Catalog.Dtos.Product;
+
+namespace MultiShop.Catalog.Services.Product;
+
+public interface IProductService
+{
+    Task<List<ProductResultDto>> GetAllAsync();
+    Task<ProductGetByIdDto> GetByIdAsync(string productId);
+    Task CreateAsync(ProductCreateDto productCreateDto);
+    Task UpdateAsync(ProductUpdateDto productUpdateDto);
+    Task DeleteAsync(string productId);
+}

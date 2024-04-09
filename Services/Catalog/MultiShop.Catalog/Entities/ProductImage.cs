fc1e6d6d3da9a38aebe294b72d2727b5c3f8a2aa
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-09 04:10:53 +0300

    Add ProductImage entity

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities;
+
+public class ProductImage
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ProductImageId { get; set; }
+
+    [BsonRepresentation(BsonType.Array)]
+    public List<string> Images { get; set; }
+
+    public string ProductId { get; set; }
+    [BsonIgnore]
+    public Product Product { get; set; }
+}

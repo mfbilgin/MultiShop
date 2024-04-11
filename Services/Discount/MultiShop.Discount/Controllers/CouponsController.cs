053b2394e7487fb7b9fca06bdbfcf47e31ce22f2
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 13:40:34 +0300

    Create coupon api controller

---
+++ Services/Discount/MultiShop.Discount/Controllers/CouponsController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CouponsController(ICouponService couponService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var coupons = await couponService.GetAllAsync();
+        return Ok(coupons);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCouponAsync(CouponCreateDto couponCreateDto)
+    {
+        await couponService.CreateCouponAsync(couponCreateDto);
+        return Ok("Coupon created successfully.");
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateCouponAsync(CouponUpdateDto couponUpdateDto)
+    {
+        await couponService.UpdateCouponAsync(couponUpdateDto);
+        return Ok("Coupon updated successfully.");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCouponAsync(int id)
+    {
+        await couponService.DeleteCouponAsync(id);
+        return Ok("Coupon deleted successfully.");
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCouponByIdAsync(int id)
+    {
+        var coupon = await couponService.GetCouponByIdAsync(id);
+        return Ok(coupon);
+    }
+}

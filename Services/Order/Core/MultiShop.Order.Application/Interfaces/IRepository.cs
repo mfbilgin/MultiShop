2457416cb74adf186270a25786c9f48993f38cee
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 20:51:08 +0300

    Create IRepository interface

---
+++ Services/Order/Core/MultiShop.Order.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Interfaces;
+
+public interface IRepository<T> where T : IEntity
+{
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<T> GetByAsync(Expression<Func<T, bool>> predicate);
+}

09e553257bbbe913e3cec103f37ae9c815e73a1c
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 21:55:14 +0300

    Create order detail results

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/OrderDetail/GetOrderDetailQueryResult.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Results.OrderDetail;
+
+public class GetOrderDetailQueryResult
+{
+    public int OrderDetailId { get; set; }
+    public string ProductId { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+    public decimal ProductPrice { get; set; }
+    public int ProductAmount { get; set; }
+    public decimal ProductTotalPrice { get; set; }
+    public int OrderId { get; set; }
+}

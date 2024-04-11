cffbb4f669574c137c01ee659f22437d035ff690
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 21:49:27 +0300

    Create orderContext

---
+++ Services/Infrastructure/MultiShop.Order.Persistence/Contexts/OrderContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistence.Contexts;
+
+public class OrderContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+    }
+
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<Domain.Entities.Order> Orders { get; set; }
+    
+}

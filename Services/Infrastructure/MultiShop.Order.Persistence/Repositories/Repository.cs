464c5d75def1d37b0391d695d2205111deb2e2ca
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 21:48:56 +0300

    Create generic repository

---
+++ Services/Infrastructure/MultiShop.Order.Persistence/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using MultiShop.Order.Persistence.Contexts;
+
+namespace MultiShop.Order.Persistence.Repositories;
+
+public class Repository<T>(OrderContext context) : IRepository<T> where T : class, IEntity
+{
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await context.Set<T>().FindAsync(id); 
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await context.Set<T>().ToListAsync();
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        context.Set<T>().Add(entity);
+        await context.SaveChangesAsync();   
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        context.Set<T>().Update(entity); 
+        await context.SaveChangesAsync();
+        
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        context.Set<T>().Remove(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<T?> GetByAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await context.Set<T>().SingleOrDefaultAsync(predicate); 
+    }
+}

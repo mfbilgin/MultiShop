96f19ca6a789ea2c9c5da7efe46be7bdb497bda9
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 20:52:11 +0300

    Create address results

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Results/Address/GetAddressByIdQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Order.Application.Features.CQRS.Results.Address;
+
+public class GetAddressByIdQueryResult
+{
+    public int AddressId { get; set; }
+    public string UserId { get; set; } = string.Empty;
+    public string District { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string Detail { get; set; } = string.Empty;
+}

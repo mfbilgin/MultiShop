30d8c1d21d3bc9d3e930c3172ccf83554b9e3184
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-11 23:36:01 +0300

    Add null control to address get by id handler

--- Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/Address/GetAddressByIdQueryHandler.cs
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/Address/GetAddressByIdQueryHandler.cs
@@ -6,16 +6,18 @@ namespace MultiShop.Order.Application.Features.CQRS.Handlers.Address;
 
 public class GetAddressByIdQueryHandler(IRepository<Domain.Entities.Address> repository)
 {
-    public async Task<GetAddressByIdQueryResult> Handle(GetAddressByIdQuery request)
+    public async Task<GetAddressByIdQueryResult?> Handle(GetAddressByIdQuery request)
     {
         var address = await repository.GetByIdAsync(request.AddressId);
-        return new GetAddressByIdQueryResult()
-        {
-            AddressId = address.AddressId,
-            UserId = address.UserId,
-            City = address.City,
-            Detail = address.City,
-            District = address.District
-        };
+        if (address != null)
+            return new GetAddressByIdQueryResult()
+            {
+                AddressId = address.AddressId,
+                UserId = address.UserId,
+                City = address.City,
+                Detail = address.City,
+                District = address.District
+            };
+        return null;
     }
 }

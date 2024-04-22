60597acab43b001d705e692799752ad9ccd5fde8
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-22 13:32:41 +0300

    Create UserRegisterDto class

---
+++ IdentityServer/MultiShop.IdentityServer/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiShop.IdentityServer.Dtos
+{
+    public sealed class UserRegisterDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}

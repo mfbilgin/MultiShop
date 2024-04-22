5ca216c26ea15646d34f8a002c4d46ff17099fc0
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-22 14:14:17 +0300

    Create RegistersController.cs

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> productUserManager)
+        {
+            _userManager = productUserManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userRegisterDto.UserName,
+                Email = userRegisterDto.Email,
+                FirstName = userRegisterDto.FirstName,
+                PhoneNumber = userRegisterDto.PhoneNumber,
+                LastName = userRegisterDto.LastName
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("User created successfully");
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}

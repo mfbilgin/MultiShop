1b1fc37708f62672e7b3a3d188bdac7a6995b387
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-22 14:02:00 +0300

    Add phone number field to register dto

--- IdentityServer/MultiShop.IdentityServer/Dtos/UserRegisterDto.cs
+++ IdentityServer/MultiShop.IdentityServer/Dtos/UserRegisterDto.cs
@@ -3,9 +3,10 @@
     public sealed class UserRegisterDto
     {
         public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Password { get; set; }
     }
 }

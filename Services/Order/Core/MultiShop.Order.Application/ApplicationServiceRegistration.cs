4d743666d97b957b2f950d7b26bfdaf6a2001957
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-04-30 21:54:36 +0300

    Remove wwwroot from repository

---
+++ Services/Order/Core/MultiShop.Order.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MultiShop.Order.Application.Features.CQRS.Handlers.Address;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetail;
+
+namespace MultiShop.Order.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<GetAddressQueryHandler>();
+        services.AddScoped<GetAddressByIdQueryHandler>();
+        services.AddScoped<CreateAddressCommandHandler>();
+        services.AddScoped<UpdateAddressCommandHandler>();
+        services.AddScoped<RemoveAddressCommandHandler>();
+
+        services.AddScoped<GetOrderDetailQueryHandler>();
+        services.AddScoped<GetOrderDetailByIdQueryHandler>();
+        services.AddScoped<CreateOrderDetailCommandHandler>();
+        services.AddScoped<UpdateOrderDetailCommandHandler>();
+        services.AddScoped<RemoveOrderDetailCommandHandler>();
+
+
+        return services;
+    }
+}
